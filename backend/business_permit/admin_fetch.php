210b0df0b9a92a1a059f7da893461454835ab318
Theijiii <orilla.maaltheabalcos@gmail.com>
2026-01-24 23:58:19 +0800

    integ trial w zoning

--- backend/business_permit/admin_fetch.php
+++ backend/business_permit/admin_fetch.php
@@ -4,7 +4,9 @@ session_start();
 
 $allowedOrigins = [
     'http://localhost',
-    'https://e-plms.goserveph.com/'
+    'https://e-plms.goserveph.com/',
+    'urbanplanning.goserveph.com',
+    'https://urbanplanning.goserveph.com'
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
